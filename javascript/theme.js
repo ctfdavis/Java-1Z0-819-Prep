8280195d4b3f2a8e555b49c1a6079d324cde6ad9
dcufo <davis.chan@aol.com>
2021-04-16 21:49:33 +0800

    added dark mode for side buttons

--- javascript/theme.js
+++ javascript/theme.js
@@ -1,9 +1,19 @@
 var darkMode;
 var toggle = 0;
 var container = document.querySelector(".container");
+var sizeControlIcons = document.querySelectorAll(".size-control__icon");
+var backToTopIcon = document.querySelector(".backToTop");
 
 const themeToggle = () => {
     container.classList.toggle("container--dark");
+    if (typeof backToTopIcon !== 'undefined') {
+        backToTopIcon.classList.toggle("backToTop--dark");
+    }
+    if (typeof sizeControlIcons !== 'undefined') {
+        sizeControlIcons.forEach( (icon) => {
+            icon.classList.toggle("size-control__icon--dark");
+        })
+    }
     if (toggle === 0) {
         localStorage.setItem('dark-mode', 'dark');
         toggle = 1;
@@ -22,5 +32,13 @@ localStorage.setItem('dark-mode', darkMode);
 
 if (localStorage.getItem('dark-mode') == 'dark') {
     container.classList.add("container--dark");
+    if (typeof backToTopIcon !== 'undefined') {
+        backToTopIcon.classList.add("backToTop--dark");
+    }
+    if (typeof sizeControlIcons !== 'undefined') {
+        sizeControlIcons.forEach( (icon) => {
+            icon.classList.add("size-control__icon--dark");
+        })
+    }
     toggle = 1;
 }
