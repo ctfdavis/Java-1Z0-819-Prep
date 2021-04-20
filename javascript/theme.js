fb711c48b9748c6730c6c9e525eeacb8bc2b1f8f
dcufo <davis.chan@aol.com>
2021-04-20 17:56:41 +0800

    added content for topic 1 and enhanced code display

--- javascript/theme.js
+++ javascript/theme.js
@@ -4,6 +4,8 @@ var container = document.querySelector(".container");
 var sizeControlIcons = document.querySelectorAll(".size-control__icon");
 var backToTopIcon = document.querySelector(".backToTop");
 var themeIcon = document.querySelector(".theme");
+var root_theme = document.querySelector(":root");
+// --link-color: #72e789;
 
 const themeToggle = () => {
     container.classList.toggle("container--dark");
@@ -12,18 +14,26 @@ const themeToggle = () => {
         backToTopIcon.classList.toggle("backToTop--dark");
     }
     if (typeof sizeControlIcons !== 'undefined') {
-        sizeControlIcons.forEach( (icon) => {
+        sizeControlIcons.forEach((icon) => {
             icon.classList.toggle("size-control__icon--dark");
         })
     }
     if (toggle === 0) {
         localStorage.setItem('dark-mode', 'dark');
+        root_theme.style.setProperty('--emphasis-color', "#f699cd");
+        root_theme.style.setProperty('--link-color', "#72e789");
+        root_theme.style.setProperty('--comment-color', '#dcdcdc');
+        root_theme.style.setProperty('--code-block', "#4b4b4b");
         toggle = 1;
     }
     else {
         localStorage.setItem('dark-mode', 'light');
+        root_theme.style.setProperty('--emphasis-color', "#f13232");
+        root_theme.style.setProperty('--link-color', "#354A12");
+        root_theme.style.setProperty('--comment-color', '#4b4b4b');
+        root_theme.style.setProperty('--code-block', "#d7fae7");
         toggle = 0;
-    }   
+    }
 }
 
 themeIcon.addEventListener("click", themeToggle);
@@ -35,11 +45,15 @@ localStorage.setItem('dark-mode', darkMode);
 if (localStorage.getItem('dark-mode') == 'dark') {
     container.classList.add("container--dark");
     themeIcon.classList.add('theme--dark');
+    root_theme.style.setProperty('--emphasis-color', "#f699cd");
+    root_theme.style.setProperty('--link-color', "#72e789");
+    root_theme.style.setProperty('--comment-color', '#dcdcdc');
+    root_theme.style.setProperty('--code-block', "#4b4b4b");
     if (backToTopIcon !== null) {
         backToTopIcon.classList.add("backToTop--dark");
     }
     if (typeof sizeControlIcons !== 'undefined') {
-        sizeControlIcons.forEach( (icon) => {
+        sizeControlIcons.forEach((icon) => {
             icon.classList.add("size-control__icon--dark");
         })
     }
